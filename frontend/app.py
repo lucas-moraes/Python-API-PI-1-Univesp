0776dc10f7f538047fd32d7fa8faeb3bbca42d74
Lucas Moraes <lucas.moraes88@yahoo.com>
2021-11-01 18:10:23 -0300

    Alteração nos nomes de alguma funções

--- frontend/app.py
+++ frontend/app.py
@@ -1,46 +1,54 @@
-from typing import List
 from flask import Flask, render_template
 import requests
 
 app = Flask(__name__)
 
+
 @app.route('/')
-def Index():
+def Home():
     return render_template('home.html')
+
 
 @app.route('/listas')
 def Listas():
 
-    lista_pacientes = requests.get(url='http://127.0.0.1:8000/listar-pacientes')
+    lista_pacientes = requests.get(
+        url='http://127.0.0.1:8000/listar-pacientes')
     listagem_pacientes_json = lista_pacientes.json()
-    pacientes = []    
+    pacientes = []
     for iten in listagem_pacientes_json:
         pacientes.append(iten)
 
-    lista_consultas = requests.get(url='http://127.0.0.1:8000/listar-consultas')
+    lista_consultas = requests.get(
+        url='http://127.0.0.1:8000/listar-consultas')
     listagem_consultas_json = lista_consultas.json()
-    consultas = []    
+    consultas = []
     for iten in listagem_consultas_json:
         consultas.append(iten)
 
     lista_medicos = requests.get(url='http://127.0.0.1:8000/listar-medicos')
     listagem_medicos_json = lista_medicos.json()
-    medicos = []    
+    medicos = []
     for iten in listagem_medicos_json:
         medicos.append(iten)
 
-    lista_especialidades_medicas = requests.get(url='http://127.0.0.1:8000/listar-especialidades-medicas')
+    lista_especialidades_medicas = requests.get(
+        url='http://127.0.0.1:8000/listar-especialidades-medicas')
     listagem_especialidades_medicas_json = lista_especialidades_medicas.json()
-    especialidades_medicas = []    
+    especialidades_medicas = []
     for iten in listagem_especialidades_medicas_json:
         especialidades_medicas.append(iten)
 
-    lista_local_atendimento = requests.get(url='http://127.0.0.1:8000/listar-local-atendimento')
+    lista_local_atendimento = requests.get(
+        url='http://127.0.0.1:8000/listar-local-atendimento')
     listagem_local_atendimento_json = lista_local_atendimento.json()
-    local_atendimento = []    
+    local_atendimento = []
     for iten in listagem_local_atendimento_json:
         local_atendimento.append(iten)
-        
-    return  render_template('listas.html', pacientes = pacientes, consultas = consultas, medicos = medicos, especialidades_medicas = especialidades_medicas, local_atendimento = local_atendimento)
 
-app.run()
+    return render_template('listas.html', pacientes=pacientes, consultas=consultas, medicos=medicos, especialidades_medicas=especialidades_medicas, local_atendimento=local_atendimento)
+
+
+if __name__ == "__main__":
+    from waitress import serve
+    serve(app.run(), host='127.0.0.1', port=6000)
