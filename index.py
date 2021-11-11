308ab72eeff671ed7de6f52b7bea6baeac1c9979
Lucas Moraes <lucas.moraes88@yahoo.com>
2021-11-10 21:43:13 -0300

    Ajuste nas chamadas do backend e frontend

--- index.py
+++
@@ -1,4 +0,0 @@
-from waitress import serve
-from frontend import app
-
-serve(app.app.run(), host='0.0.0.0', port=8080)
---
+++ run_backend.py
@@ -0,0 +1,11 @@
+from backend.view import app
+import uvicorn
+
+
+uvicorn.run(
+    app,
+    host="127.0.0.1",
+    port=8000,
+    ssl_keyfile="./backend/cert/localhost+2-key.pem",
+    ssl_certfile="./backend/cert/localhost+2.pem"
+)
